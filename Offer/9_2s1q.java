e8434ec18f0d66665b1b40a0b32dd06223153e7a
keith <mrgitservice@outlook.com>
2019-04-08 16:26:30 +0800

    Update 9.java

--- Offer/9_2s1q.java
+++ Offer/9_2s1q.java
@@ -3,19 +3,20 @@ import java.util.Stack;
 public class Solution {
     Stack<Integer> stack1 = new Stack<Integer>();
     Stack<Integer> stack2 = new Stack<Integer>();
-
+    
     public void push(int node) {
         stack1.push(node);
     }
 
     public int pop() {
-        while (!stack1.empty()) {
-            stack2.push(stack1.pop());
+        if(!stack2.isEmpty()) {
+            return stack2.pop();
         }
-        int res = stack2.pop();
-        while (!stack2.isEmpty()) {
-            stack1.push(stack2.pop());
+        else {
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+            return stack2.pop();
         }
-        return res;
     }
 }
